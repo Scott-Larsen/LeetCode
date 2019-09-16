1507653e206dd564f06691ec27b93f3e0ca9fde8
Scott <scott@scottlarsen.com>
2019-09-16 00:50:41 -0700

    Add 448. Find All Numbers Disappeared in an Array

---
+++ 448-FindAllNumbersDisappearedinanArray.py
@@ -0,0 +1,14 @@
+# Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some
+# elements appear twice and others appear once.
+
+# Find all the elements of [1, n] inclusive that do not appear in this array.
+
+# Could you do it without extra space and in O(n) runtime? You may assume the
+# returned list does not count as extra space.
+
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        full = set(range(1, len(nums) + 1))
+        disappeared = full - set(nums)
+        return disappeared
