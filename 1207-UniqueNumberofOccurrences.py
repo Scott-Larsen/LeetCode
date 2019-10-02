dbea253c56070d69567634b224b52551916295a0
Scott <scott@scottlarsen.com>
2019-10-02 13:22:50 -0700

    Add 1207. Unique Number of Occurrences

---
+++ 1207-UniqueNumberofOccurrences.py
@@ -0,0 +1,15 @@
+# Given an array of integers arr, write a function that returns true if and only
+# if the number of occurrences of each value in the array is unique.
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        d = collections.defaultdict(int)
+        for x in arr:
+            d[x] += 1
+        occurrences = []
+        for key in d.keys():
+            if d[key] != 0 and d[key] not in occurrences:
+                occurrences.append(d[key])
+            else:
+                return False
+        return True
