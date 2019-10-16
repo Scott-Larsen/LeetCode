3ed5d3418cbdcc9242c5f81f457c80633fb83126
Scott <scott@scottlarsen.com>
2019-10-15 22:39:01 -0700

    Add 1122. Relative Sort Array

---
+++ 1122-RelativeSortArray.py
@@ -0,0 +1,16 @@
+# Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all
+# elements in arr2 are also in arr1.
+
+# Sort the elements of arr1 such that the relative ordering of items in arr1 are
+# the same as in arr2.  Elements that don't appear in arr2 should be placed at
+# the end of arr1 in ascending order.
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        a = []
+        for item in arr2:
+            while item in arr1:
+                arr1.remove(item)
+                a.append(item)
+        arr1.sort()
+        return a + arr1
