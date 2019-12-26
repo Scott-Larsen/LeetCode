a3c89a3f4280eb6ad2b56f2e17e53dd337f7c732
Scott <scott@scottlarsen.com>
2019-12-25 19:52:59 -0800

    Add 1233. Remove Sub-Folders from the Filesystem

---
+++ 1233-RemoveSub-FoldersfromtheFilesystem.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/
+
+# Given a list of folders, remove all sub-folders in those folders and return in
+# any order the folders after removing.
+
+# If a folder[i] is located within another folder[j], it is called a sub-folder
+# of it.
+
+# The format of a path is one or more concatenated strings of the form: /
+# followed by one or more lowercase English letters. For example, /leetcode and
+# /leetcode/problems are valid paths while an empty string and / are not.
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort(key = len, reverse=True)
+        res = [folder.pop()]
+        while folder:
+            f = folder.pop()
+            entry, f = f[:], f[1:]
+            f = f.split('/')
+            for i in range(len(f)):
+                if '/' + '/'.join(f[0:i + 1]) in res:
+                    break
+                elif i == len(f) - 1:
+                    res.append(entry)
+        return res
