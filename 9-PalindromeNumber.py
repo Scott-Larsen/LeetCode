5709243f50cf71b925b6b7481bd657bc27ab8df0
Scott <scott@scottlarsen.com>
2020-02-05 08:46:43 -0800

    Update 9. Palindrome Number

--- 9-PalindromeNumber.py
+++ 9-PalindromeNumber.py
@@ -5,9 +5,23 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        while len(x) > 1:
-            if x[0] != x[-1]:
-                return False
-            x = x[1:-1]
-        return True
+        numbers = []
+        if x < 0:
+            return False
+        elif x == 0:
+            return True
+        else:
+            while x > 0:
+                newX = int(x / 10)
+                numbers.append(x - newX * 10)
+                x = newX
+            return True if numbers == numbers[::-1] else False
+        return False            
+        
+        # Done with strings
+        # x = str(x)
+        # while len(x) > 1:
+        #     if x[0] != x[-1]:
+        #         return False
+        #     x = x[1:-1]
+        # return True
