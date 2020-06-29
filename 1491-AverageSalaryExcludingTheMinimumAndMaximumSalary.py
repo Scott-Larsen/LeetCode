677b480f2b551f3fcaa312b008c7928e1ed3d09d
Scott-Larsen <scott@scottlarsen.com>
2020-06-28 23:12:14 -0400

    Add 1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ 1491-AverageSalaryExcludingTheMinimumAndMaximumSalary.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+
+# Given an array of unique integers salary where salary[i] is the salary of the employee i.
+
+# Return the average salary of employees excluding the minimum and maximum salary.
+
+ 
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary.sort()
+        return sum(salary[1:-1])/ len(salary[1:-1])
